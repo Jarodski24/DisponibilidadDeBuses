606035cce1dc74ac69fd1693d0fb2b0de7efba28
Jarodski24 <thomasjarod2403@gmail.com>
2026-08-21 00:27:11 -0600

    debug

--- js/email.js
+++ js/email.js
@@ -1,54 +1,38 @@
 /* =========================================================
-   BUSUCR - EMAILJS
+   BUSUCR - ENVÍO DE CORREO
 ========================================================= */
 
+/* =========================================================
+   CONFIGURACIÓN
+========================================================= */
 console.log("================================");
 console.log("EMAIL.JS SE ESTA EJECUTANDO");
 console.log("================================");
 
 
-/* =========================================================
-   CONFIGURACIÓN
-========================================================= */
+const EMAILJS_PUBLIC_KEY = "QJPaB6kqAIjI0ESpT";
 
-const EMAILJS_PUBLIC_KEY = "TU_PUBLIC_KEY";
-const EMAILJS_SERVICE_ID = "TU_SERVICE_ID";
-const EMAILJS_TEMPLATE_ID = "TU_TEMPLATE_ID";
+const EMAILJS_SERVICE_ID = "serviceBusUCR";
 
-
-console.log("Public Key:", EMAILJS_PUBLIC_KEY);
-console.log("Service ID:", EMAILJS_SERVICE_ID);
-console.log("Template ID:", EMAILJS_TEMPLATE_ID);
+const EMAILJS_TEMPLATE_ID = "template_k4acgtc";
 
 
 /* =========================================================
-   COMPROBAR EMAILJS
-========================================================= */
-
-console.log("EmailJS disponible:", typeof emailjs);
-
-
-/* =========================================================
-   INICIALIZAR
+   INICIALIZAR EMAILJS
 ========================================================= */
 
 emailjs.init({
-    publicKey: EMAILJS_PUBLIC_KEY
-});
 
-console.log("EmailJS inicializado correctamente");
+    publicKey: EMAILJS_PUBLIC_KEY
+
+});
 
 
 /* =========================================================
-   FUNCIÓN ENVIAR CORREO
+   FUNCIÓN PARA ENVIAR CORREO
 ========================================================= */
 
 function enviarCorreo(nombre, correo, carnet) {
-
-    console.log("================================");
-    console.log("ENVIAR CORREO FUE EJECUTADO");
-    console.log("================================");
-
 
     const datos = {
 
@@ -61,11 +45,6 @@ function enviarCorreo(nombre, correo, carnet) {
     };
 
 
-    console.log("Datos enviados a EmailJS:");
-
-    console.log(datos);
-
-
     return emailjs.send(
 
         EMAILJS_SERVICE_ID,
@@ -74,34 +53,6 @@ function enviarCorreo(nombre, correo, carnet) {
 
         datos
 
-    )
-
-    .then(function(response) {
-
-        console.log("================================");
-        console.log("EMAILJS RESPONDIÓ CORRECTAMENTE");
-        console.log("================================");
-
-        console.log("Status:", response.status);
-
-        console.log("Text:", response.text);
-
-
-        return response;
-
-    })
-
-    .catch(function(error) {
-
-        console.error("================================");
-        console.error("ERROR DE EMAILJS");
-        console.error("================================");
-
-        console.error(error);
-
-
-        throw error;
-
-    });
+    );
 
 }
